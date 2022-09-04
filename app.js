6cf5bfb1413d9046c81efc48146fd1dda9d699fe
Raven <reevan0613@gmail.com>
2022-09-04 16:36:56 +0800

    feat: add detail page and login authentication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const usePassport = require('./config/passport')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
@@ -6,9 +7,6 @@ const app = express()
 const PORT = 3000
 const routes = require('./routes');
 
-const db = require('./models')
-const Todo = db.Todo
-const User = db.User
 
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
@@ -17,7 +15,7 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
+usePassport(app)
 
 app.use(routes);
 
--- routes/module/user.js
+++ routes/module/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 const db = require('../../models')
@@ -9,19 +10,43 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  res.send('login')
-})
+//登入功能
+router.post('/login', passport.authenticate('local' , {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
+//註冊功能
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
+
 
 router.get('/logout', (req, res) => {
   res.send('logout')
