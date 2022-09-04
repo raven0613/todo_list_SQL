762cfa7b3e0e6a705cd3a44c1002c6c098c432e5
Raven <reevan0613@gmail.com>
2022-09-04 15:32:18 +0800

    feat: finish homepage

--- routes/module/home.js
+++ routes/module/home.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  res.render('index');
+  return Todo
+  .findAll({
+    raw: true,
+    nest: true
+  })
+  .then((todos) => { return res.render('index', { todos: todos }) })
+  .catch((error) => { return res.status(422).json(error) })
 })
 
 
